e75b92d81f8bbce5de59a43bc51b96d7f27bdd17
nekohor <nekohor@foxmail.com>
2017-10-24 11:03:01 +0800

    Manipu

--- manipulation.py
+++ manipulation.py
@@ -23,9 +23,9 @@ def pos(std, side, access):
             .format(std=std, side=side, access=access))
 
 
-def slp(side, access, how):
-    return ("k_{side}_{access}_{how}"
-            .format(side=side, access=access, how=how))
+def slp(side, access, how, times):
+    return ("k_{side}_{access}_{how}_{times}"
+            .format(side=side, access=access, how=how, times=times))
 
 
 def progressbar(i):
@@ -33,47 +33,48 @@ def progressbar(i):
     sys.stdout.write('第{0}机架计算中！- 总共7机架\r'.format(i + 1))
     sys.stdout.flush()
 
+
 # --- setup para ---
 # data directory
-
-
 root_dir = "../data/零调数据"
+
 last_num_list = [-1, -2, -3]
-
-last_num = -1
-
-
-summary = pd.DataFrame()
-
-
-data_dir = "/".join([root_dir, os.listdir(root_dir)[last_num]])
-
-
-times_id = "A"
-
-file_name = [x for x in os.listdir(data_dir) if times_id in x][0]
-
-
-df = pd.read_csv("/".join([data_dir, file_name]))
-
-print(df)
+times_id_list = ["A", "B", "C", "D", "E", "F"]
 std_list = [1, 2, 3, 4, 5, 6, 7]
 side_list = ["OS", "DS"]
 access_list = ["ENT", "EXT"]
 how_list = ["LC", "PT"]
 level_line = 4000
-for std in std_list:
-    df = df.loc[df[frc(std, "OS", "LC")] > level_line]
-    for side in side_list:
-        for access in access_list:
-            for how in how_list:
-                summary[std, slp(side, access, how)] = (
 
-                    np.polyfit(df[pos(std, side, access)],
-                               slp(), 1)
-                )
-        np.polyfit(X, Y, 1)
-    # first a as an example
+
+last_num = -1
+
+summary = pd.DataFrame()
+last_date = os.listdir(root_dir)[last_num]
+data_dir = "/".join([root_dir, last_date])
+for times_id in times_id_list:
+    print(times_id)
+    file_name = [x for x in os.listdir(data_dir) if times_id in x][0]
+    df = pd.read_csv("/".join([data_dir, file_name]))
+    for std in std_list:
+        df = df.loc[df[frc(std, "OS", "LC")] > level_line]
+        for side in side_list:
+            for access in access_list:
+                for how in how_list:
+                    pos_size = df[pos(std, side, access)].shape[0]
+                    frc_size = df[frc(std, side, how)].shape[0]
+                    if ((pos_size != 0) and (frc_size != 0) and
+                            (pos_size == frc_size)):
+                        summary[std, slp(side, access, how, times_id)] = (
+                            np.polyfit(df[pos(std, side, access)],
+                                       df[frc(std, side, how)], 1)[0]
+                        )
+                    else:
+                        summary[std, slp(side, access, how, times_id)] = 0
+
+
+summary.to_excel("result.xlsx")
+# first a as an example
 
 # def
 # df = pd.read_csv("grade.csv")
