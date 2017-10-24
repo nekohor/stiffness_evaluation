ec34cd1fcce6ac9da238a30c64bd8c2215d079ed
nekohor <nekohor@foxmail.com>
2017-10-25 00:23:45 +0800

    “Complete!!!!

--- manipulation.py
+++ manipulation.py
@@ -4,28 +4,40 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 import os
-import shutil
 import sys
-import time
+import seaborn as sns  # 学院派风格的话，这个用不到
+import docx   # 注意是python-docx
+from docx.shared import Inches
 matplotlib.style.use('ggplot')
 
-# plt.rcParams["font.sans-serif"] = ["SimHei"]
-# plt.rcParams["axes.unicode_minus"] = False
+plt.rcParams["font.sans-serif"] = ["Microsoft Yahei"]
+plt.rcParams["axes.unicode_minus"] = False
+
+# sns.set(color_codes=True)
+# sns.set(rc={'font.family': [u'Microsoft YaHei']})
+# sns.set(rc={'font.sans-serif': [u'Microsoft YaHei', u'Arial',
+#                                 u'Liberation Sans', u'Bitstream Vera Sans',
+#                                 u'sans-serif']})
 
 
 def frc(std, side, how):
-    return ("L_F{std}H_{side}RLFCFBK{how}"
+    return ("L_{std}H_{side}RLFCFBK{how}"
             .format(std=std, side=side, how=how))
 
 
 def pos(std, side, access):
-    return ("L_F{std}H_{side}{access}POSFBK"
+    return ("L_{std}H_{side}{access}POSFBK"
             .format(std=std, side=side, access=access))
 
 
 def slp(side, access, how, times):
     return ("k_{side}_{access}_{how}_{times}"
             .format(side=side, access=access, how=how, times=times))
+
+
+def box_col(std, the_date):
+    return ("{std}_{the_date}"
+            .format(std=std, the_date=the_date))
 
 
 def progressbar(i):
@@ -38,67 +50,124 @@ def progressbar(i):
 # data directory
 root_dir = "../data/零调数据"
 
-last_num_list = [-1, -2, -3]
+date_num_list = [-1, -2, -3]
 times_id_list = ["A", "B", "C", "D", "E", "F"]
-std_list = [1, 2, 3, 4, 5, 6, 7]
+std_list = ["F1", "F2", "F3", "F4", "F5", "F6", "F7"]
 side_list = ["OS", "DS"]
 access_list = ["ENT", "EXT"]
 how_list = ["LC", "PT"]
 level_line = 4000
 
-
 last_num = -1
-
-summary = pd.DataFrame()
 last_date = os.listdir(root_dir)[last_num]
-data_dir = "/".join([root_dir, last_date])
-for times_id in times_id_list:
-    print(times_id)
-    file_name = [x for x in os.listdir(data_dir) if times_id in x][0]
-    df = pd.read_csv("/".join([data_dir, file_name]))
-    for std in std_list:
-        df = df.loc[df[frc(std, "OS", "LC")] > level_line]
-        for side in side_list:
-            for access in access_list:
-                for how in how_list:
-                    pos_size = df[pos(std, side, access)].shape[0]
-                    frc_size = df[frc(std, side, how)].shape[0]
-                    if ((pos_size != 0) and (frc_size != 0) and
-                            (pos_size == frc_size)):
-                        summary[std, slp(side, access, how, times_id)] = (
-                            np.polyfit(df[pos(std, side, access)],
-                                       df[frc(std, side, how)], 1)[0]
-                        )
-                    else:
-                        summary[std, slp(side, access, how, times_id)] = 0
 
+box_data = pd.DataFrame()
 
-summary.to_excel("result.xlsx")
-# first a as an example
+for date_num in date_num_list:
+    summary = pd.DataFrame()
+    the_date = os.listdir(root_dir)[date_num]
+    data_dir = "/".join([root_dir, the_date])
+    for times_id in times_id_list:
+        # print(times_id)
+        file_name = [x for x in os.listdir(data_dir) if times_id in x][0]
+        df = pd.read_csv("/".join([data_dir, file_name]))
+        for std in std_list:
+            df = df.loc[df[frc(std, "OS", "LC")] > level_line]
+            for side in side_list:
+                for access in access_list:
+                    for how in how_list:
+                        pos_size = df[pos(std, side, access)].shape[0]
+                        frc_size = df[frc(std, side, how)].shape[0]
+                        if ((pos_size != 0) and (frc_size != 0) and
+                                (pos_size == frc_size)):
+                            summary.loc[std,
+                                        slp(side, access, how, times_id)] = (
+                                np.polyfit(df[pos(std, side, access)],
+                                           df[frc(std, side, how)], 1)[0]
+                            )
+                        else:
+                            summary.loc[std, slp(
+                                side, access, how, times_id)] = 0
 
-# def
-# df = pd.read_csv("grade.csv")
-# print(df)
-# df["grade"].plot(kind="bar")
-# plt.savefig("hehe.png")
+    if not os.path.exists("../data/inter/{}".format(the_date)):
+        os.makedirs("../data/inter/{}".format(the_date))
+    if not os.path.exists("../data/pic/{}".format(the_date)):
+        os.makedirs("../data/pic/{}".format(the_date))
+    summary.to_excel("../data/inter/{}/中间计算结果.xlsx".format(the_date))
 
+    plot_data = pd.DataFrame()
+    for how in how_list:
+        for std in std_list:
+            the_col_list = [x for x in summary.columns if how in x]
+            the_series = summary.loc[std, the_col_list]
+            the_series = the_series.loc[the_series > 0.001]
+            if the_series.shape[0] != 0:
+                plot_data.loc[std, how] = round(
+                    the_series.mean() / 3000 * 100, 2)
+            else:
+                plot_data.loc[std, how] = 0
 
-# df4 = pd.DataFrame({'wyy': np.random.randn(1000) + 1,
-#                     'zyx': np.random.randn(1000),
-#                     'yx': np.random.randn(1000) - 1},
-#                    columns=['wyy', 'zyx', 'yx'])
+    # --- plt.fig ---
+    plt.figure(0)
+    plot_data.plot(kind="bar")
+    plt.title = "LC和PT刚度比较"
+    plt.xlabel("机架")
+    plt.ylabel("刚度保持率%")
 
+    x = np.arange(len(plot_data.index))
+    y1 = plot_data["LC"]
+    y2 = plot_data["PT"]
+    for a, b in zip(x, y1):
+        plt.text(a - 0.3, b + 0.05, '%.1f' %
+                 b, ha='center', va='bottom', fontsize=10)
+    for a, b in zip(x, y2):
+        plt.text(a + 0.3, b + 0.05, '%.1f' %
+                 b, ha='center', va='bottom', fontsize=10)
+    plt.savefig("../data/pic/{}/bar_plot.png".format(the_date))
+    plt.close(0)
 
-# df = pd.read_csv("grade.csv")
-# print(df)
-# df["grade"].plot(kind="bar")
-# plt.savefig("hehe.png")
+    # 全机架保持率计算
+    how = "LC"
+    for times_id in times_id_list:
+        for std in std_list:
+            the_col_list = [
+                x for x in summary.columns if times_id in x if how in x]
+            the_series = summary.loc[std, the_col_list]
+            the_series = the_series.loc[the_series > 0.001]
+            if the_series.shape[0] != 0:
+                box_data.loc[times_id, box_col(std, the_date)] = round(
+                    the_series.mean() / 3000 * 100, 2)
+            else:
+                box_data.loc[times_id, box_col(std, the_date)] = np.nan
+                # or 0
 
+box_data.to_excel("../data/inter/{}/box计算结果.xlsx".format(last_date))
 
-# df4.plot(kind='hist', alpha=0.5)
-# plt.savefig("1.png")
+for std in std_list:
+    the_col_list = [
+        x for x in box_data.columns if std in x]
+    plt.figure(0)
+    box_data[the_col_list].boxplot()
+    # print(std)
+    plt.title = "{}机架刚度保持率".format(std)
+    plt.xlabel("日期时间")
+    plt.ylabel("刚度保持率%")
+    plt.savefig("../data/pic/{}/box_plot_{}.png".format(last_date, std))
+    plt.close(0)
 
+doc = docx.Document("base.docx")
+doc.add_paragraph("刚度评价{}".format(last_date))
+doc.paragraphs[-1].style = "Title"
 
-# df = pd.DataFrame(np.random.randn(1000, 2), columns=['a', 'b'])
-# df.plot(kind='hexbin', x='a', y='b', gridsize=25)
-# plt.savefig("2.png")
+doc.add_paragraph("    LC和PT刚度如下图所示。")
+doc.add_picture("../data/pic/{}/bar_plot.png".format(last_date),
+                width=Inches(6.2))
+for std in std_list:
+    doc.add_paragraph("   {}机架近3周刚度保持率如下图所示。".format(std))
+    doc.add_picture(
+        "../data/pic/{}/box_plot_{}.png".format(last_date, std),
+        width=Inches(6.2))
+
+if not os.path.exists("../data/result"):
+    os.makedirs("../data/result")
+doc.save("../data/result/轧机刚度评价{}.docx".format(last_date))
